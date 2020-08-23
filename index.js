16c93c6b10c7bffdec2dd98d7a607ded6cb9b342
Joe Cui <joe.cui@bpanz.com>
2020-08-23 16:42:30 +1000

    Added routing and HTML templating

--- index.js
+++ index.js
@@ -29,18 +29,51 @@ const url = require("url");
 // ================= SERVER ==================== //
 
 // at top level, use sync code becaues it only run once - don't read file on each request
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"); // __dirname is the current folder
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%NUTURIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 // parse json data into JavaScript object
 const dataObj = JSON.parse(data);
 
+// server is called each time when a request comes in
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
 
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
-  } else if (pathName === "/api") {
+  // use "url" module to extract pathname and query string from req.url
+  // console.log(url.parse(req.url, true));
+  const { query, pathname } = url.parse(req.url, true); 
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+  // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id]; // use query string to find object in array json
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  // API
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   } else {
