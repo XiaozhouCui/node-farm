23adb76a7ab084f8263d7919ca256ab831f58e56
Joe Cui <joe.cui@bpanz.com>
2020-08-23 14:07:02 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Non-blocking, asynchronous way (callback hell)
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log(err);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+
+
+// ================= SERVER ==================== //
+
+// at top level, use sync code becaues it only run once - don't read file on each request
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"); // __dirname is the current folder
+// parse json data into JavaScript object
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html", "my-own-header": "hello-world" });
+    res.end("Page not found!");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on prot 8000");
+});
